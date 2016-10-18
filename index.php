8ba660271a27c15be95f1fcff9a5ed0d0883b2eb
sumet-nutrition <com-sc521463124@hotmail.com>
2016-10-18 09:45:46 +0700

    ทดสอบ

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        // put your code here
+        $car=['a'=>23,'b'=>34,'c'=>56];
+            print_r($car);
+            echo '<hr>';
+            
+            
+            foreach ($car as $k=>$v) {
+                    //echo $height['robert'];
+            echo 'แสดงค่า :'.$k.' / '.$v.'<br>';
+            echo '<hr>';
+    
+}
+          
+               // echo $car[$x].'<br>';
+            
+            
+        ?>
+    </body>
+</html>
